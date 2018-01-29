0e548d8de4fcc15f8c3c60d369d1494a0742045d
qinhuai <1504707743@qq.com>
2018-01-29 22:50:34 +0800

    完成标签条的设计

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,118 @@
                         <div class="swiper-button-prev swiper-button-white"></div>
                         <div class="swiper-button-next swiper-button-white"></div>
                     </div>
+                    <!--推荐专题-->
+                    <div class="recommend-collection">
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection1.jpg" alt="">
+                            <span>文学</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection2.jpg" alt="">
+                            <span>人文</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection3.jpg" alt="">
+                            <span>故事</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection1.jpg" alt="">
+                            <span>文学</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection2.jpg" alt="">
+                            <span>人文</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection3.jpg" alt="">
+                            <span>故事</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection1.jpg" alt="">
+                            <span>文学</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection2.jpg" alt="">
+                            <span>人文</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection3.jpg" alt="">
+                            <span>故事</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection1.jpg" alt="">
+                            <span>文学</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection2.jpg" alt="">
+                            <span>人文</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection3.jpg" alt="">
+                            <span>故事</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection1.jpg" alt="">
+                            <span>文学</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collection/123">
+                            <img src="../assets/img/collection2.jpg" alt="">
+                            <span>人文</span>
+                        </nuxt-link>
+                        <nuxt-link to="/collecyion123" class="more-hot-collection">
+                            更多热门
+                            <i class="fa fa-angle-right"></i>
+                        </nuxt-link>
+                    </div>
+                    <p class="split-line">
+                    </p>
+                    <!--文章列表-->
+                    <div class="list-container">
+                        <div class="note-list">
+                            <li class="have-img">
+                                <nuxt-link class="wrap-img" to="/p/123">
+                                    <img src="../assets/img/containersmpic.jpg" alt="">
+                                </nuxt-link>
+                                <div class="content">
+                                    <div class="auther">
+                                        <nuxt-link to="/u/123" class="avatar">
+                                            <img src="../assets/img/default-avatar.jpg" alt="">
+                                        </nuxt-link>
+                                        <div class="info">
+                                            <nuxt-link to="/u/123" class="nickname">
+                                                测试用户
+                                            </nuxt-link>
+                                            <span class="time">8小时前</span>
+                                        </div>
+                                    </div>
+                                    <nuxt-link class="title" to="/p/123">
+                                        如何把书中的只是转化为能力？这是我见过的最好的答案
+                                    </nuxt-link>
+                                    <p class="abstract">
+                                        随着知识经济的兴起，读书的重要性得到了大家的普遍认同，于是市场上出现很多读书方法，比如速度、多读、慢读、列读……不管你采用哪一种读书方法，最终都要回归到“行动”二字上，正如陆...
+
+                                    </p>
+                                    <div class="meta">
+                                        <nuxt-link to="/collection/123" class="collection-tag">
+                                        短篇小说
+                                        </nuxt-link>
+                                        <nuxt-link class="tag" to="/p/123">
+                                            <i class="fa fa-eye"></i>
+                                            579
+                                        </nuxt-link>
+                                        <nuxt-link class="tag" to="/p/123">
+                                            <i class="fa fa-comment"></i>
+                                            200
+                                        </nuxt-link>
+                                        <span>
+                                            <i class="fa fa-heart"></i>
+                                            546
+                                        </span>
+                                    </div>
+                                </div>
+                            </li>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-4 aside">
                     <div class="board">
